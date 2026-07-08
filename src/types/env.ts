4c215c053d8660ce6631add30528baa99cfd5e42
READTalk <redtalk.app@hotmail.com>
2026-07-08 21:14:42 +0700

    Update env.ts

--- src/types/env.ts
+++ src/types/env.ts
@@ -1,5 +1,8 @@
 export type Env = {
     AUTHORIZATION_DURABLE_OBJECT: DurableObjectNamespace;
     CONVERSATION_DURABLE_OBJECT: DurableObjectNamespace;
+    DATABASE: D1Database;        
+    SESSION_CACHE: KVNamespace;  
+    FEED_CACHE: KVNamespace;     
     MESSAGE_ASSETS: R2Bucket;
-} 
+}
